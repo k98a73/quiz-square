993ab553d78a851d9329b2a96baeb9c1353080fd
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-07-11 15:28:34 +0900

    ヘッダーのログイン状態における認証アイコンの表示切り替えを追加

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
+  Avatar,
   ButtonGroup,
   Flex,
   Heading,
@@ -10,8 +11,49 @@ import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import { HiOutlineUserAdd } from "react-icons/hi";
 import NextLink from "next/link";
 import useSignOut from "../hooks/useSignOut";
+import { auth, db } from "../lib/firebase";
+
+const SignOutContainer = ({uid}: any) => {
+  const [avatarUrl, setAvatarUrl] = useState<any>("");
+  const docRef = db.collection("users").doc(uid);
+  docRef
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        setAvatarUrl(doc.data()?.imageUrl);
+      } else {
+        alert("No such document!");
+      }
+    })
+    .catch((error) => {
+      alert(error);
+    });
+
+  return (
+    <>
+      <Avatar ml="3" size="md" src={avatarUrl} />
+      <IconButton
+        aria-label="signOut"
+        bg="cyan.600"
+        color="gray.50"
+        rounded="full"
+        size="lg"
+        icon={<FaSignOutAlt />}
+        onClick={useSignOut()}
+      />
+    </>
+  );
+};
 
 const Header = () => {
+  const [user, setUser] = useState<any>("");
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+  }, []);
+
   return (
     <>
       <Flex
@@ -27,37 +69,34 @@ const Header = () => {
         </NextLink>
         <Spacer />
         <ButtonGroup mr="5" gap="2">
-          <NextLink href="/signup" passHref>
-            <IconButton
-              aria-label="signUp"
-              bg="cyan.600"
-              color="gray.50"
-              rounded="full"
-              size="lg"
-              as="a"
-              icon={<HiOutlineUserAdd />}
-            />
-          </NextLink>
-          <NextLink href="/signin" passHref>
-            <IconButton
-              aria-label="signIn"
-              bg="cyan.600"
-              color="gray.50"
-              rounded="full"
-              size="lg"
-              as="a"
-              icon={<FaSignInAlt />}
-            />
-          </NextLink>
-          <IconButton
-            aria-label="signOut"
-            bg="cyan.600"
-            color="gray.50"
-            rounded="full"
-            size="lg"
-            icon={<FaSignOutAlt />}
-            onClick={useSignOut()}
-          />
+          {user ? (
+            <SignOutContainer uid={user?.uid} />
+          ) : (
+            <>
+              <NextLink href="/signup" passHref>
+                <IconButton
+                  aria-label="signUp"
+                  bg="cyan.600"
+                  color="gray.50"
+                  rounded="full"
+                  size="lg"
+                  as="a"
+                  icon={<HiOutlineUserAdd />}
+                />
+              </NextLink>
+              <NextLink href="/signin" passHref>
+                <IconButton
+                  aria-label="signIn"
+                  bg="cyan.600"
+                  color="gray.50"
+                  rounded="full"
+                  size="lg"
+                  as="a"
+                  icon={<FaSignInAlt />}
+                />
+              </NextLink>
+            </>
+          )}
         </ButtonGroup>
       </Flex>
     </>
