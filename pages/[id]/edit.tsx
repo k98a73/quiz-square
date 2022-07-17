9ef23a97e5d63141917cdf3c854e5eeef02fbdc6
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-07-17 11:42:03 +0900

    クイズの作成者しかクイズ編集画面にアクセスできない様に変更

--- pages/[id]/edit.tsx
+++ pages/[id]/edit.tsx
@@ -1,27 +1,48 @@
-import React from "react";
+import { Center, Spinner } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 import { useRecoilValue } from "recoil";
 
 import QuizInputForm from "../../components/QuizInputForm";
 import { quizItemState } from "../../constans/atom";
+import useIsMounted from "../../hooks/useIsMounted";
+import { auth } from "../../lib/firebase";
 
 export default function QuizEdit() {
   const quizItem = useRecoilValue(quizItemState);
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+  // マウントを監視するカスタムフック
+  const isMountedRef = useIsMounted();
+
+  auth.onAuthStateChanged((user) => {
+    if (isMountedRef && user?.uid !== quizItem.uid) {
+      router.push("/");
+    } else {
+      setLoading(false);
+    }
+  });
 
   return (
     <>
-      <QuizInputForm
-        quizID={quizItem.id}
-        genreDefaultValue={quizItem.genre}
-        contentDefaultValue={quizItem.content}
-        optionADefaultValue={quizItem.optionA}
-        optionBDefaultValue={quizItem.optionB}
-        optionCDefaultValue={quizItem.optionC}
-        optionDDefaultValue={quizItem.optionD}
-        answerDefaultValue={quizItem.answer}
-        descriptionDefaultValue={quizItem.description}
-        buttonSentence={"問題の更新"}
-      />
-      ;
+      {loading ? (
+        <Center mt="30%">
+          <Spinner size="xl" />
+        </Center>
+      ) : (
+        <QuizInputForm
+          quizID={quizItem.id}
+          genreDefaultValue={quizItem.genre}
+          contentDefaultValue={quizItem.content}
+          optionADefaultValue={quizItem.optionA}
+          optionBDefaultValue={quizItem.optionB}
+          optionCDefaultValue={quizItem.optionC}
+          optionDDefaultValue={quizItem.optionD}
+          answerDefaultValue={quizItem.answer}
+          descriptionDefaultValue={quizItem.description}
+          buttonSentence={"問題の更新"}
+        />
+      )}
     </>
   );
 }
