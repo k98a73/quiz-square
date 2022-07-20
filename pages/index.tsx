fe3e23a572da63972a1dd87994ceb0b9487e6e95
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-07-20 15:43:29 +0900

    ウェルカムページにレスポンシブ対応

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Box, Container, Stack, VStack } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Header from "../components/Header";
@@ -9,6 +10,21 @@ const Home: NextPage = () => {
         <title>Quiz Square</title>
       </Head>
       <Header />
+      <Container py="3" maxW="800px">
+        <VStack>
+          <Stack direction={["column", "row"]} spacing="3">
+            <Box w="full" h="100px" bgColor="teal.400" p="2">
+              I am Box.
+            </Box>
+            <Box w="full" h="100px" bgColor="teal.400" p="2">
+              I am Box.
+            </Box>
+            <Box w="full" h="100px" bgColor="teal.400" p="2">
+              I am Box.
+            </Box>
+          </Stack>
+        </VStack>
+      </Container>
     </>
   );
 };
