f6c6cbefb68eba77df6df6f9092daa9e8942f06b
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-08-13 16:53:43 +0900

    #12 ログイン画面をFirebase v9対応に変更

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -18,6 +18,7 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 import Header from "../components/Header";
 import { auth } from "../lib/firebase";
@@ -45,13 +46,12 @@ export default function SignIn() {
 
   const handleClick = () => setShow(!show);
 
-  const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
-    try {
-      await auth.signInWithEmailAndPassword(email, password);
-      router.push("/quizzesIndex");
-    } catch (error: any) {
-      alert(error.message);
-    }
+  const onSubmit: SubmitHandler<Inputs> = ({ email, password }) => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => router.push("/quizzesIndex"))
+      .catch((error) => {
+        alert(error.message);
+      });
   };
 
   return (
